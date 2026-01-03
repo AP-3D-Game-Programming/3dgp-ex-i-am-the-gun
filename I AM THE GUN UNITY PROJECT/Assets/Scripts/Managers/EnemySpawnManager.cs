4b0ffc71124c2a7c9386d374347ad149d15a5bfc
Wout Oris <woutoris7@gmail.com>
2026-01-03 14:37:32 +0100

    Add SpawnManager script

---
+++ I AM THE GUN UNITY PROJECT/Assets/Scripts/Managers/EnemySpawnManager.cs
@@ -0,0 +1,37 @@
+using NUnit.Framework;
+using UnityEngine;
+
+public class EnemySpawnManager : MonoBehaviour
+{
+    [SerializeField] GameObject[] levelEnemies;
+    [SerializeField] GameObject[] spawners;
+    [SerializeField] float spawnInterval = 5;
+    private float timer = 0;
+    [SerializeField] int amountPerSpawn;
+    private GameManager gameManager;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        spawners = GameObject.FindGameObjectsWithTag("Spawner");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!gameManager.gameIsPaused)
+        {
+            timer += Time.deltaTime;
+
+            if (timer >= spawnInterval)
+            {
+                for (int i = 0; i < amountPerSpawn; i++)
+                {
+                    GameObject.Instantiate(levelEnemies[Random.Range(0, levelEnemies.Length)], spawners[Random.Range(0, spawners.Length)].transform);
+                }
+                timer = 0;
+            }
+        }
+    }
+}
